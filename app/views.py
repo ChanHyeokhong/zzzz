e88022708fd34ff47ab762a20bf6b025cceafc90
HongChanHyeok <hchc5783@naver.com>
2018-04-25 19:56:58 +0900

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.http import JsonResponse
+# Create your views here.
+
+
+def keyboard(request):
+
+    return JsonResponse(
+        {
+            'type': 'buttons',
+            'buttons': ['학식']
+        }
+    )
+
